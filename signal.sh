7a3ec8595a4cd732c45e20ad275040f20483ea65
Armando Hernandez <11252662+armhzjz@users.noreply.github.com>
2020-08-23 23:17:07 +0200

    correct name of docker image

--- signal.sh
+++ signal.sh
@@ -4,5 +4,5 @@ docker run -d --user $(id -u):$(id -g) --rm -it -e HOME \
   -e DISPLAY=unix:0 -e XAUTHORITY=/tmp/xauth \
   -v $XAUTHORITY:/tmp/xauth -v $HOME:$HOME \
   -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro \
-  -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --network=host -w $HOME armhzjz/sigindock:latest
+  -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --network=host -w $HOME armhzjz/signal-in-docker:latest
 
